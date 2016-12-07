e0a237aea2653131d999b8cf3984a3ad8ad34e10
vip65394@yandex.ru <6539465394>
2016-12-08 00:33:24 +0300

    tiles set up

--- src/main/java/shop/Application.java
+++ src/main/java/shop/Application.java
@@ -3,6 +3,11 @@ package shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 import shop.setup.Data;
 
 @SpringBootApplication(scanBasePackages = "shop")
@@ -12,6 +17,21 @@ public class Application {
         ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
         Data data = ctx.getBean(Data.class);
         data.setUp();
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tiles = new TilesConfigurer();
+        tiles.setDefinitions("/WEB-INF/layout/tiles.xml");
+        tiles.setCheckRefresh(true);
+        return tiles;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        TilesViewResolver tilesViewResolver = new TilesViewResolver();
+        tilesViewResolver.setViewClass(TilesView.class);
+        return tilesViewResolver;
     }
 
 }
